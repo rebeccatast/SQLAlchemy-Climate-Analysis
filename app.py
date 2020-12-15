8869fc534146dc42ac244358d9ca57d01496cc79
Rebecca Tast <69356535+rebeccatast@users.noreply.github.com>
2020-12-14 21:50:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 # 1. import Flask
 from flask import Flask
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
+import numpy as np
+import pandas as pd
+engine = create_engine ("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine,reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session=Session(engine)
 
 # 2. Create an app, being sure to pass __name__
 app = Flask(__name__)
@@ -8,10 +21,11 @@ app = Flask(__name__)
 # 3. Define what to do when a user hits the index route
 @app.route("/")
 def home():
-    print("Server received request for 'Home' page...")
+    return (
+        print("Server received request for 'Home' page...")
     return "Welcome to my 'Home' page!"
-
-
+    )
+    
 # 4. Define what to do when a user hits the /about route
 @app.route("/about")
 def about():
