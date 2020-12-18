93420e831ab2dd81b12693cd62cd3319ea630f07
Rebecca Tast <69356535+rebeccatast@users.noreply.github.com>
2020-12-17 22:58:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -68,18 +68,111 @@ def stations():
 
     #Return list of all stations#
     #Query all stations
-    results = session.query(Station.station).\
-        order_by(Station.station).all()
+    results = session.query(Station.station, Station.name).all()
+    for s, name in results:
+        station[s] = name
     
     session.close()
 
-    #Convert list of tuples into normal list
+    return jsonify(stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    #Create session link from Python to DB
+    session = Session(engine)
+
+    #Get last date 
+    last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    last_year_date = (dt.datetime.strptime(last_date[0], '%Y-%m-%d') 
+    - dt.timedelta(days =365)).strftime('%Y-%m-%d')
+
+    #Query for the dates and temps
+    results = session.query(Measurement.date, Measurement.tobs)
+    filter(Measurement.date >= last_year_date). \
+            order_by(Measurement.date).all()
+    tobs_date_list = []
+
+    for date, tobs in results:
+        new_dict={}
+        new_dict[date] = tobs
+        tobs_date_list.append(new_dict)
     
+    session.close()
+
+    return jsonify (tobs_date_list)
+    
+@app.route("/api/v1.0/<start>")
+def temp_range_start(start):
+    """TMIN, TAVG, and TMAX per date starting from a starting date.
+    
+    Args:
+        start (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+
+     #Create session link from Python to DB
+    session = Session(engine)
+
+    return_list = []
+
+    results =   session.query(  Measurement.date,\
+                                func.min(Measurement.tobs), \
+                                func.avg(Measurement.tobs), \
+                                func.max(Measurement.tobs)).\
+                        filter(Measurement.date >= start).\
+                        group_by(Measurement.date).all()
+
+    for date, min, avg, max in results:
+        new_dict = {}
+        new_dict["Date"] = date
+        new_dict["TMIN"] = min
+        new_dict["TAVG"] = avg
+        new_dict["TMAX"] = max
+        return_list.append(new_dict)
+
+    session.close()    
+
+    return jsonify(return_list)
+
+@app.route("/api/v1.0/<start>/<end>")
+def temp_range_start_end(start,end):
+    """TMIN, TAVG, and TMAX per date for a date range.
+    
+    Args:
+        start (string): A date string in the format %Y-%m-%d
+        end (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+
+    #Create session link from Python to DB
+    session = Session(engine)
+
+    return_list = []
+
+    results =  session.query(  Measurement.date,\
+                                func.min(Measurement.tobs), \
+                                func.avg(Measurement.tobs), \
+                                func.max(Measurement.tobs)).\
+                        filter(and_(Measurement.date >= start, Measurement.date <= end)).\
+                        group_by(Measurement.date).all()
+
+    for date, min, avg, max in results:
+        new_dict = {}
+        new_dict["Date"] = date
+        new_dict["TMIN"] = min
+        new_dict["TAVG"] = avg
+        new_dict["TMAX"] = max
+        return_list.append(new_dict)
+
+    session.close()    
+
+    return jsonify(return_list)
 
 
-
-
-    #
 # 4. Define what to do when a user hits the /about route
 @app.route("/about")
 def about():
